19effbadb7da56c0352f06e5fd8937acf772bb4c
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-08-20 01:32:20 +0100

    finished footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./pub/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': { 'max': '767px' },
+      'md': { 'min': '768px', 'max': '1023px' },
+    },
     extend: {},
   },
   plugins: [],
