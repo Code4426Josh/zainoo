aafe4a961060ba055fdb63d4abfc9b686620ea42
Code4426Josh <chukwuebukajosh8@gmail.com>
2023-08-27 01:10:25 +0100

    responsive

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 /* ---------------------------- getting elements ---------------------------- */
 const hamburger = document.querySelector('.hamburger');
-// console.log('hamburger: ', hamburger);
 const listCon = document.querySelector('.lists');
-console.log('listCon: ', listCon);
+const navLinks = document.querySelectorAll('#click');
 
 /* --------------------------- hamburger function --------------------------- */
 hamburger.addEventListener('click', () => { 
+  hamburger.classList.toggle('active')
   listCon.classList.toggle('show-list');
 });
+/* --------------------------- remove class when click link --------------------------- */
+navLinks.forEach(function (clickers) {
+  clickers.addEventListener('click', () => {
+    listCon.classList.remove('show-list');
+    hamburger.classList.remove('active')
+  });
+})
+
